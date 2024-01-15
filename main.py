7a7f55c49c9574344cc001d0fcb6406b5d6042ae
James B <74630856+jmsbell@users.noreply.github.com>
2024-01-15 13:26:57 -0800

    Update main.py
    New better version

--- main.py
+++ main.py
@@ -8,27 +8,27 @@ def main():
     Count from 1 to last number.
     Answer with "fizz" if divisible by 3, "buzz" if divisible by 5, "fizzbuzz" if divisible by 3 and 5
     """
-    last_number = int(input("Number to count to: "))
+    last_number = get_valid_input("Number to count to? ")
     fizzcount = 0
     buzzcount = 0 
     fizzbuzzcount = 0
 
-    for i in range(1, last_number +1):
-        fizz = (i % 3 == 0) and (i!=0)
-        buzz = (i % 5 == 0) and (i!=0)
-        fizzbuzz = (i % 15 == 0) and (i!=0)
+    for i in range(1, last_number + 1):
+        fizz = (i % 3 == 0) and (i != 0)
+        buzz = (i % 5 == 0) and (i != 0)
+        fizzbuzz = (i % 15 == 0) and (i != 0)
         if fizzbuzz:
             print("Fizzbuzz")
-            #add to fizzbuzz total
-            fizzbuzzcount = fizzbuzzcount +1
+            # add to fizzbuzz total
+            fizzbuzzcount += 1
         elif fizz:
             print("Fizz")
-            #add to fizz total
-            fizzcount = fizzcount +1
+            # add to fizz total
+            fizzcount += 1
         elif buzz:
             print("Buzz")
             # add to buzz total
-            buzzcount = buzzcount +1
+            buzzcount += 1
         else:
             print(i)
 
@@ -36,9 +36,24 @@ def main():
     print('Num fizzed: ' + str(fizzcount))
     print('Num buzzed: ' + str(buzzcount))
     print('Num fizzbuzzed: '+ str(fizzbuzzcount))
+    print('')
 
-    
+def get_valid_input(prompt): #this is a nice validation loop to check the inputs. It does run both number_of_turns & lsat_number
+    while True:
+        user_input = input(prompt)
+        try:
+            number = int(user_input)
+            return number
+        except ValueError:
+            print('')
+            print("Please enter a valid integer.")
+            print('')
 
- 
+def turns():
+    number_of_turns = get_valid_input("How many times do you want to run Fizzbuzz? ")
+    print('')
+    for _ in range(number_of_turns):
+        main()
+
 if __name__ == '__main__':
-    main()
+    turns()
